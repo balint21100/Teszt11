93e16d4dcf716306c089b8ffe9ab924a67c151f6
Sima Tímea <105370870+Timeroon@users.noreply.github.com>
2026-05-03 03:09:34 +0200

    Statisztika grafikon

--- Teszt1/Frontend/Statisztika/StatisztikaPageViewModel.cs
+++ Teszt1/Frontend/Statisztika/StatisztikaPageViewModel.cs
@@ -1,106 +1,81 @@
-﻿
-using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using Teszt1.Bakckend.Calsses;
-using Teszt1.Bakckend.Database;
-using Teszt1.Bakckend.Services;
-// ... többi using marad
+using Microsoft.Maui.Graphics;
 
 namespace Teszt1.Frontend.Statisztika
 {
-
     public class GrafikonOszlop
     {
-        public int Magassag { get; set; }
+        public double Magassag { get; set; }
         public string Cimke { get; set; }
-        public string Szin { get; set; }
+        public Color Szin { get; set; }
     }
+
     public class TopListaElem
     {
         public int Helyezes { get; set; }
         public string Nev { get; set; }
         public string ErtekSzoveg { get; set; }
         public string HelyezesText => $"{Helyezes}.";
-        public string HatterSzin => Helyezes == 1 ? "#FFD700" : (Helyezes == 2 ? "#C0C0C0" : (Helyezes == 3 ? "#CD7F32" : "#B0000000"));
-        public string SzovegSzin => Helyezes <= 3 ? "Black" : "White";
+        public Color HatterSzin => Helyezes == 1 ? Color.FromArgb("#FFD700") :
+                                  (Helyezes == 2 ? Color.FromArgb("#C0C0C0") :
+                                  (Helyezes == 3 ? Color.FromArgb("#CD7F32") : Color.FromRgba(255, 255, 255, 0.2)));
+        public Color SzovegSzin => Helyezes <= 3 ? Colors.Black : Colors.White;
     }
 
     public partial class StatisztikaPageViewModel : ObservableObject
     {
-        private readonly WorkoutService _workoutService;
-        private readonly MealService _mealService;
-
         public StatisztikaPageViewModel()
         {
             FrissitsAdatokat();
         }
-        [ObservableProperty] private bool isEdzesAktiv = true;
-        [ObservableProperty] private string edzesTabSzin = "#6200EE";
-        [ObservableProperty] private string etkezesTabSzin = "Transparent";
 
+        [ObservableProperty] private bool isEdzesAktiv = true;
         [ObservableProperty] private string aktualisIdotav = "Heti";
-        [ObservableProperty] private string hetiSzin = "#6200EE";
-        [ObservableProperty] private string haviSzin = "Transparent";
-        [ObservableProperty] private string evesSzin = "Transparent";
-        [ObservableProperty] private string egyeniSzin = "Transparent";
+
+        [ObservableProperty] private Color edzesTabSzin = Color.FromArgb("#6200EE");
+        [ObservableProperty] private Color etkezesTabSzin = Colors.Transparent;
+
+        [ObservableProperty] private Color hetiSzin = Color.FromArgb("#6200EE");
+        [ObservableProperty] private Color haviSzin = Colors.Transparent;
+        [ObservableProperty] private Color evesSzin = Colors.Transparent;
+        [ObservableProperty] private Color egyeniSzin = Colors.Transparent;
 
         [ObservableProperty] private bool isHetiLathato = true;
         [ObservableProperty] private bool isHaviLathato = false;
         [ObservableProperty] private bool isEvesLathato = false;
         [ObservableProperty] private bool isEgyeniLathato = false;
 
-
         public List<string> Hetek { get; set; } = new() { "Aktuális hét", "Előző hét", "2 héttel ezelőtt" };
         public List<string> Honapok { get; set; } = new() { "Január", "Február", "Március", "Április", "Május", "Június", "Július", "Augusztus", "Szeptember", "Október", "November", "December" };
         public List<string> Evek { get; set; } = new() { "2024", "2025", "2026" };
 
         [ObservableProperty] private string kivalasztottHet = "Aktuális hét";
-        [ObservableProperty] private string kivalasztottHonap = "Április";
+        [ObservableProperty] private string kivalasztottHonap = "Május";
         [ObservableProperty] private string kivalasztottEv = "2026";
         [ObservableProperty] private DateTime kezdoDatum = DateTime.Now.AddDays(-7);
         [ObservableProperty] private DateTime vegDatum = DateTime.Now;
 
-        [ObservableProperty] private string osszesitoCimke1 = "Összesen";
-        [ObservableProperty] private string osszesitoErtek1 = "0";
-        [ObservableProperty] private string osszesitoCimke2 = "Átlagosan";
-        [ObservableProperty] private string osszesitoErtek2 = "0";
+        [ObservableProperty] private string osszesitoCimke1;
+        [ObservableProperty] private string osszesitoErtek1;
+        [ObservableProperty] private string osszesitoCimke2;
+        [ObservableProperty] private string osszesitoErtek2;
 
-        [ObservableProperty] private string aktualisCim = "";
-        [ObservableProperty] private string yMaxSzoveg = "0";
-        [ObservableProperty] private string yMidSzoveg = "0";
-        [ObservableProperty] private string topListaCim = "Top Lista";
+        [ObservableProperty] private string aktualisCim;
+        [ObservableProperty] private string yMaxSzoveg;
+        [ObservableProperty] private string yMidSzoveg;
+        [ObservableProperty] private string topListaCim;
 
         public ObservableCollection<GrafikonOszlop> GrafikonOszlopok { get; set; } = new();
         public ObservableCollection<TopListaElem> TopLista { get; set; } = new();
 
-
-        // Aktuális napok száma tárolva a frissítéshez
-        private int _aktualisNapok = 7;
-
-        public StatisztikaPageViewModel(WorkoutService workoutService, MealService mealService)
-        {
-            _workoutService = workoutService;
-            _mealService = mealService;
-            // Kezdeti adatok betöltése
-            FrissitsAdatokat();
-        }
         [RelayCommand]
         public void TabValtas(string tab)
         {
-            if (tab == "Edzes")
-            {
-                IsEdzesAktiv = true;
-                EdzesTabSzin = "#6200EE"; EtkezesTabSzin = "Transparent";
-            }
-            else
-            {
-                IsEdzesAktiv = false;
-                EdzesTabSzin = "Transparent"; EtkezesTabSzin = "#6200EE";
-            }
+            IsEdzesAktiv = (tab == "Edzes");
+            EdzesTabSzin = IsEdzesAktiv ? Color.FromArgb("#6200EE") : Colors.Transparent;
+            EtkezesTabSzin = !IsEdzesAktiv ? Color.FromArgb("#6200EE") : Colors.Transparent;
             FrissitsAdatokat();
         }
 
@@ -108,88 +83,105 @@ namespace Teszt1.Frontend.Statisztika
         public void IdotavValtas(string idotav)
         {
             AktualisIdotav = idotav;
-            HetiSzin = HaviSzin = EvesSzin = EgyeniSzin = "Transparent";
+            HetiSzin = HaviSzin = EvesSzin = EgyeniSzin = Colors.Transparent;
             IsHetiLathato = IsHaviLathato = IsEvesLathato = IsEgyeniLathato = false;
 
-            if (idotav == "Heti") { HetiSzin = "#6200EE"; IsHetiLathato = true; }
-            else if (idotav == "Havi") { HaviSzin = "#6200EE"; IsHaviLathato = true; }
-            else if (idotav == "Eves") { EvesSzin = "#6200EE"; IsEvesLathato = true; }
-            else if (idotav == "Egyeni") { EgyeniSzin = "#6200EE"; IsEgyeniLathato = true; }
-
+            switch (idotav)
+            {
+                case "Heti": HetiSzin = Color.FromArgb("#6200EE"); IsHetiLathato = true; break;
+                case "Havi": HaviSzin = Color.FromArgb("#6200EE"); IsHaviLathato = true; break;
+                case "Eves": EvesSzin = Color.FromArgb("#6200EE"); IsEvesLathato = true; break;
+                case "Egyeni": EgyeniSzin = Color.FromArgb("#6200EE"); IsEgyeniLathato = true; break;
+            }
             FrissitsAdatokat();
         }
 
         [RelayCommand]
         public void FrissitsAdatokat()
         {
-            int userId = 1; // IDEIGLENES: Itt a bejelentkezett User ID-ja kell!
-            GrafikonOszlopok.Clear();
-            TopLista.Clear();
-            var rnd = new Random();
-            string alapSzin = IsEdzesAktiv ? "#00E676" : "#FF9800";
-
-            if (!IsEdzesAktiv)
+            MainThread.BeginInvokeOnMainThread(() =>
             {
-                AktualisCim = "Kalóriabevitel elemzés";
-                OsszesitoCimke1 = "Összes bevitt kalória";
-                OsszesitoErtek1 = rnd.Next(15000, 25000).ToString("N0") + " kcal";
-                OsszesitoCimke2 = "Napi átlag kalória";
-                OsszesitoErtek2 = rnd.Next(2000, 3500).ToString("N0") + " kcal";
+                GrafikonOszlopok.Clear();
+                TopLista.Clear();
 
-                TopListaCim = "Top 10 leggyakoribb étel";
-                List<string> etelek = new() { "Csirkemell", "Rizs", "Zabpehely", "Tojás", "Protein Shake", "Banán", "Édesburgonya", "Túró", "Alma", "Mandula" };
-                for (int i = 0; i < 10; i++)
-                    TopLista.Add(new TopListaElem { Helyezes = i + 1, Nev = etelek[i], ErtekSzoveg = $"{rnd.Next(400, 4500)} kcal" });
-                GeneraloMotor(7, new[] { "H", "K", "Sze", "Cs", "P", "Szo", "V" }, 1500, 4000, alapSzin);
+                var rnd = new Random();
+                Color alapSzin = IsEdzesAktiv ? Color.FromArgb("#00E676") : Color.FromArgb("#FF9800");
 
-            }
-            else
-            {
-                AktualisCim = "Gyakorlatok terhelése";
-                OsszesitoCimke1 = "Összes megmozgatott súly";
-                OsszesitoErtek1 = rnd.Next(40000, 120000).ToString("N0") + " kg";
-                OsszesitoCimke2 = "Átlagos terhelés";
-                OsszesitoErtek2 = rnd.Next(5000, 15000).ToString("N0") + " kg";
-
-                TopListaCim = "Top 10 leggyakoribb edzés";
-                List<string> gyakorlatok = new() { "Fekvenyomás", "Guggolás", "Felhúzás", "Mellről nyomás", "Evezés", "Bicepsz", "Húzódzkodás", "Tricepsz", "Kitörés", "Vádli" };
-                int[] topSulyok = new int[10];
-                for (int i = 0; i < 10; i++)
+                if (!IsEdzesAktiv)
                 {
-                    int suly = rnd.Next(10, 80);
-                    topSulyok[i] = suly;
-                    TopLista.Add(new TopListaElem { Helyezes = i + 1, Nev = gyakorlatok[i], ErtekSzoveg = $"Össz: {suly}kg | Átlag: {suly / 4}kg" });
+                    AktualisCim = "Kalóriabevitel elemzés";
+                    OsszesitoCimke1 = "Összes bevitt kalória";
+                    OsszesitoErtek1 = rnd.Next(15000, 25000).ToString("N0") + " kcal";
+                    OsszesitoCimke2 = "Napi átlag kalória";
+                    OsszesitoErtek2 = rnd.Next(2000, 3500).ToString("N0") + " kcal";
+                    TopListaCim = "Top 5 leggyakoribb étel";
+
+                    string[] etelek = { "Csirkemell", "Rizs", "Zabpehely", "Tojás", "Protein", "Banán", "Édesburgonya" };
+                    var rendezettEtelek = etelek.Select(e => new { Nev = e, Darab = rnd.Next(5, 45) })
+                                                .OrderByDescending(x => x.Darab)
+                                                .ToList();
+
+                    for (int i = 0; i < Math.Min(5, rendezettEtelek.Count); i++)
+                    {
+                        TopLista.Add(new TopListaElem
+                        {
+                            Helyezes = i + 1,
+                            Nev = rendezettEtelek[i].Nev,
+                            ErtekSzoveg = $"{rendezettEtelek[i].Darab} alkalommal"
+                        });
+                    }
+
+                    RajzoldKiGrafikont(new int[] { 2100, 1850, 2400, 2200, 1900, 2600, 2150 }, new[] { "H", "K", "Sze", "Cs", "P", "Szo", "V" }, alapSzin);
                 }
-                GeneraloMotorSpecialis(10, gyakorlatok.Select(x => x.Substring(0, 3)).ToArray(), topSulyok, alapSzin);
-            }
-        }
-        private void GeneraloMotor(int darab, string[] cimkek, int min, int max, string szin)
-        {
-            var rnd = new Random();
-            int[] ertekek = new int[darab];
-            for (int i = 0; i < darab; i++) ertekek[i] = rnd.Next(min, max);
-            RajzoldKiGrafikont(ertekek, cimkek, szin);
+                else
+                {
+                    AktualisCim = "Gyakorlatok terhelése";
+                    OsszesitoCimke1 = "Összes súly";
+                    OsszesitoErtek1 = rnd.Next(40000, 100000).ToString("N0") + " kg";
+                    OsszesitoCimke2 = "Átlagos napi";
+                    OsszesitoErtek2 = rnd.Next(5000, 12000).ToString("N0") + " kg";
+                    TopListaCim = "Top 5 leggyakoribb gyakorlat";
+
+                    string[] gyakorlatok = { "Fekvenyomás", "Guggolás", "Felhúzás", "Evezés", "Bicepsz", "Tricepsz", "Kitörés" };
+                    var rendezettGyakorlatok = gyakorlatok.Select(g => new { Nev = g, Darab = rnd.Next(2, 25) })
+                                                          .OrderByDescending(x => x.Darab)
+                                                          .ToList();
+
+                    for (int i = 0; i < Math.Min(5, rendezettGyakorlatok.Count); i++)
+                    {
+                        TopLista.Add(new TopListaElem
+                        {
+                            Helyezes = i + 1,
+                            Nev = rendezettGyakorlatok[i].Nev,
+                            ErtekSzoveg = $"{rendezettGyakorlatok[i].Darab} alkalommal"
+                        });
+                    }
+
+                    RajzoldKiGrafikont(new int[] { 80, 120, 150, 100, 90, 110, 70 }, new[] { "Fekv", "Gugg", "Felh", "Evez", "Bic", "Tri", "Váll" }, alapSzin);
+                }
+            });
         }
 
-        private void GeneraloMotorSpecialis(int darab, string[] cimkek, int[] ertekek, string szin)
+        private void RajzoldKiGrafikont(int[] ertekek, string[] cimkek, Color szin)
         {
-            RajzoldKiGrafikont(ertekek, cimkek, szin);
-        }
-        private void RajzoldKiGrafikont(int[] ertekek, string[] cimkek, string szin)
-        {
-            float maxErtek = ertekek.Max();
-            YMaxSzoveg =  maxErtek.ToString();
-            YMidSzoveg =  (maxErtek / 2).ToString();
-            int[] asd = new int[ertekek.Length];
-            for (int i = 0; i < asd.Length; i++)
-            {
-                asd[i] = Convert.ToInt32(ertekek[i]);
-            }
+            if (ertekek.Length == 0) return;
+            double maxErtek = ertekek.Max();
+            if (maxErtek == 0) maxErtek = 1;
 
-            double szorzo = 180 / maxErtek;
+            YMaxSzoveg = maxErtek.ToString("N0");
+            YMidSzoveg = (maxErtek / 2).ToString("N0");
+
+            double maxMegjelenithetoMagassag = 150;
+            double szorzo = maxMegjelenithetoMagassag / maxErtek;
+
             for (int i = 0; i < ertekek.Length; i++)
             {
-                GrafikonOszlopok.Add(new GrafikonOszlop { Magassag = (int)(asd[i] * szorzo), Cimke = i < cimkek.Length ? cimkek[i] : (i + 1).ToString(), Szin = szin });
+                GrafikonOszlopok.Add(new GrafikonOszlop
+                {
+                    Magassag = ertekek[i] * szorzo,
+                    Cimke = i < cimkek.Length ? cimkek[i] : (i + 1).ToString(),
+                    Szin = szin
+                });
             }
         }
     }
